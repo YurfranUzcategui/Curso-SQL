1a880d177309c3128e7abd0949353fe9cc4a35f0
YurfranUzcategui <94582830+YurfranUzcategui@users.noreply.github.com>
2023-01-22 21:56:03 -0300

    Add files via upload

---
+++ scrips-SQL+Uzcategui.sql
@@ -0,0 +1,63 @@
+create database tienda_de_abarrotes;
+
+use tienda_de_abarrotes;
+
+
+CREATE TABLE if not exists tiendas (
+ID_Tienda int auto_increment not null primary key,
+ID_Vendedor int,
+Nombre varchar(24) NOT NULL,
+Numero int not null,
+Direccion varchar(40) NOT NULL
+);
+alter table tiendas add foreign key (ID_Vendedor) references vendedores(ID_Vendedor);
+
+CREATE TABLE if not exists proveedores (
+ID_Proveedor int auto_increment not null primary key,
+ID_Tienda int,
+Nombre varchar(24) NOT NULL,
+Apellido varchar(24) NOT NULL,
+DNI int NOT NULL unique,
+Numero int not null,
+foreign key (ID_Tienda) references tiendas(ID_Tienda)
+);
+
+CREATE TABLE if not exists vendedores (
+ID_Vendedor int auto_increment not null primary key,
+ID_Tienda int,
+Nombre varchar(24) NOT NULL,
+Apellido varchar(24) NOT NULL,
+DNI int NOT NULL unique,
+Domicilio varchar(40) NOT NULL,
+foreign key (ID_Tienda) references tiendas(ID_Tienda)
+);
+
+Create table if not exists productos (
+ID_Producto int auto_increment not null primary key,
+ID_Proveedor int,
+Valor numeric,
+Cantidad numeric,
+Descripcion varchar (25),
+foreign key (ID_Proveedor) references proveedores(ID_Proveedor)
+);
+
+Create table if not exists clientes (
+ID_Cliente int auto_increment not null primary key,
+Nombre varchar (24) NOT NULL,
+Apellido varchar (24) NOT NULL,
+DNI int NOT NULL unique
+);
+
+Create table facturacion (
+ID_Facturacion int auto_increment not null primary key,
+ID_Tienda int,
+ID_Vendedor int,
+ID_Cliente int,
+ID_Producto int,
+foreign key (ID_Tienda) references tiendas(ID_Tienda),
+foreign key (ID_Vendedor) references vendedores(ID_Vendedor),
+foreign key (ID_Cliente) references clientes(ID_Cliente),
+foreign key (ID_Producto) references productos(ID_Producto)
+);
+
+
